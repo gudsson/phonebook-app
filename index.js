4e6d2ddb097dcd0965bb29e13433fbe6c425c503
Jay Gudsson <gudsson@gmail.com>
2021-07-30 01:23:31 -0700

    3.16 complete

--- index.js
+++ index.js
@@ -54,19 +54,27 @@ app.get('/api/persons', (request, response) => {
   })
 })
 
-app.get('/api/persons/:id', (request, response) => {
-  Person.findById(request.params.id).then(person => {
-    response.json(person)
-  })
+app.get('/api/persons/:id', (request, response, next) => {
+  Person.findById(request.params.id)
+    .then(person => {
+      if (person) {
+        response.json(person)
+      } else {
+        response.status(404).end()
+      }
+    })
+    .catch(error => next(error))
 })
 
-app.delete('/api/persons/:id', (request, response) => {
-  Person.findByIdAndRemove(request.params.id).then(person => {
-    response.json(person)
-  })
+app.delete('/api/persons/:id', (request, response, next) => {
+  Person.findByIdAndRemove(request.params.id)
+    .then(result => {
+      response.status(204).end()
+    })
+    .catch(error => next(error))
 })
 
-app.put('/api/persons/:id', (request, response) => {
+app.put('/api/persons/:id', (request, response, next) => {
   const body = request.body
   const id = request.params.id
 
@@ -75,9 +83,11 @@ app.put('/api/persons/:id', (request, response) => {
     number: body.number,
   })
 
-  Person.findByIdAndUpdate(id, body, { new: true }).then(savedPerson => {
-    response.json(savedPerson)
-  })
+  Person.findByIdAndUpdate(id, body, { new: true })
+    .then(updatedPerson => {
+      response.json(updatedPerson)
+    })
+    .catch(error => next(error))
 })
 
 app.post('/api/persons', (request, response) => {
@@ -97,24 +107,25 @@ app.post('/api/persons', (request, response) => {
   person.save().then(savedPerson => {
     response.json(savedPerson)
   })
-
-  // Person.find({}).then(people => {
-  //   if (people.some(person => person.name === body.name)) {
-  //     return response.status(400).json({ 
-  //       error: 'name must be unique'
-  //     })
-  //   } else {
-    // const person = new Person({
-    //   name: body.name,
-    //   number: body.number,
-    // })
-  
-    // person.save().then(savedPerson => {
-    //   response.json(savedPerson)
-    // })
-  //   }
-  // })
 })
+
+const unknownEndpoint = (request, response) => {
+  response.status(404).send({ error: 'unknown endpoint' })
+}
+
+app.use(unknownEndpoint)
+
+const errorHandler = (error, request, response, next) => {
+  console.error(error.message)
+
+  if (error.name === 'CastError') {
+    return response.status(400).send({ error: 'malformatted id' })
+  } 
+
+  next(error)
+}
+
+app.use(errorHandler)
 
 const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
